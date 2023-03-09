9fa9945b76a9f18ab0b4a9566c382df464afc452
xandecoelho5 <xandecoelho5@hotmail.com>
2023-03-09 19:06:51 -0300

    added config server

---
+++ ConfigServer/src/test/java/com/xandecoelho5/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xandecoelho5.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
